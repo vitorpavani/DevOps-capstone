77e6ed738d3dc27040821d857557da60f8e80fa0
Vitor Pavan <vipavani@hotmail.com>
2021-07-22 01:16:54 +0200

    remove useeffect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import Login from './pages/Login';
