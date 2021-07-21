fe6116ef19aa00b540f81390f5ec28f2c088f6e4
Vitor Pavan <vipavani@hotmail.com>
2021-07-21 21:42:52 +0200

    Revert "updated frontend"
    This reverts commit cef7327c6da1f547e2171ccf60a571a1c010738a.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,111 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-import { Route, Redirect } from "react-router-dom";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-// import Logout from "./services/auth";
-import Profile from "./components/Profile";
-import NewArticle from "./components/NewArticle";
-import Market from "./components/Market";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import UserPage from "./components/UserPage";
-import ArticleView from "./components/ArticleView";
-import ArticleEdit from "./components/ArticleEdit";
-import Feed from "./components/Feed";
-import LandingPage from "./components/LandingPage";
-// import Article from "../../models/Article";
+import logo from './logo.svg';
+import './App.css';
 
-// import deskImage from "./images/working-desk.png";
-
-class App extends Component {
-  state = {
-    user: this.props.user,
-  };
-
-  setUser = (user) => {
-    this.setState({
-      user: user,
-    });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <Navbar user={this.state.user} setUser={this.setUser} />
-
-        <Route
-          exact
-          path="/"
-          render={(props) => <LandingPage user={this.state.user} />}
-        />
-
-        <Route
-          exact
-          path="/article"
-          render={(props) => {
-            if (this.state.user) {
-              return <NewArticle {...props} user={this.state.user} />;
-            } else {
-              return <Redirect to="/login" />;
-            }
-          }}
-        />
-        <Route
-          exact
-          path="/userpage/:id"
-          render={(props) => <UserPage {...props} user={this.state.user} />}
-        />
-
-        <Route
-          exact
-          path="/article/:id"
-          render={(props) => <ArticleView {...props} user={this.state.user} />}
-        />
-
-        <Route
-          exact
-          path="/article/edit/:id"
-          render={(props) => {
-            if (this.state.user) {
-              return <ArticleEdit {...props} user={this.state.user} />;
-            } else {
-              return <Redirect to="/login" />;
-            }
-          }}
-        />
-
-        <Route
-          exact
-          path="/signup"
-          render={(props) => <Signup setUser={this.setUser} {...props} />}
-        />
-        <Route
-          exact
-          path="/login"
-          render={(props) => <Login setUser={this.setUser} {...props} />}
-        />
-        <Route
-          exact
-          path="/profile"
-          render={(props) => <Profile setUser={this.setUser} {...props} />}
-        />
-        <Route
-          exact
-          path="/market"
-          render={(props) => <Market setUser={this.setUser} {...props} />}
-        />
-
-        <Route
-          exact
-          path="/feed"
-          render={(props) => <Feed {...props} user={this.state.user} />}
-        />
-
-        <Footer user={this.state.user} setUser={this.setUser} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
