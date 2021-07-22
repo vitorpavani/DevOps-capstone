97b70b3a0e35075ff50caa3f8ea5337046c20680
Vitor Pavan <vipavani@hotmail.com>
2021-07-22 12:23:20 +0200

    change port and workflowId

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
